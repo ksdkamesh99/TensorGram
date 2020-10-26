b4223cc2296ad68ae44cf8c1b3bfda25bb747b43
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-10-26 18:27:43 +0530

    Setup file added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tensorgram",
+    version="0.0.1",
+    author="Sai Durga Kamesh Kota",
+    author_email="ksdkamesh99@gmail.com",
+    description="A realtime remote service to get the keras callbacks to the telegram including the details of metrics ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ksdkamesh99/TensorGramBot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
